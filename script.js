eb164df5051256919a0b9fb406e77602a623be25
vishal0316 <vr987195@gmail.com>
2023-08-15 13:46:17 +0530

    changes

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
 
+    var timer = 2;
+    var score = 0;
+    var hitrn = 0;
 
-var timer = 60;
-var score = 0;
-
-function increaseScore() {
-    score 
-}
-
-
-function getNewHit() {
-    var rn = Math.floor(Math.random() * 10);
-    document.querySelector("#hitval").textContent = rn;
-}
-
-
-
-function makeBubble(){
-    var clutter = "";
-
-    for(var i = 1;  i<=168 ; i++){
-       var rn =  Math.floor(Math.random()*10)
-        clutter +=  `<div class="bubble">${rn}</div>` ;
+    function increaseScore(){
+        score += 5;
+        document.querySelector("#scoreval").textContent = score;
     }
+
+
+    function NewHit(){
+         hitrn = Math.floor(Math.random()*10);
+        document.querySelector("#hitval").textContent = hitrn;
+    }
+
+    function makeBubble() {
+        var clutter = "";
+
+        for (var i = 1; i <= 168; i++) {
+            var rn = Math.floor(Math.random() * 10)
+            clutter += `<div class="bubble">${rn}</div>`;
+        }
+
+        document.querySelector(".pbtm").innerHTML = clutter;
+    }
+
+    function runTimer() {
+        var timerint = setInterval(function() {
+            if (timer > 0) {
+                timer--;
+                document.querySelector("#timerval").textContent = timer;
+            } else {
+                clearInterval(timerint);
+                document.querySelector(".pbtm").innerHTML = `<h1> Game Over </h1>`;
+            }
+        }, 1000);
+    }
+
+    document.querySelector(".pbtm")
+    .addEventListener("click", function (detail) {
+        var clickednum = Number(detail.target.textContent);
+        if (clickednum === hitrn) {
+            increaseScore();
+            makeBubble();
+            NewHit();
+        }
+    });
+
+    runTimer();
+    makeBubble();
+    NewHit();
+    increaseScore();
     
-    document.querySelector(".pbtm").innerHTML = clutter;
-}
- 
-
-
-function runTimer() {
- var timerint = setInterval(function(){
-    if (timer > -0){
-    timer--;
-    document.querySelector("#timerval").textContent = timer;
-    }
-    else{
-        clearInterval(timerint);
-    }
-}, 1000);
-}
-
-
-
-runTimer();
-makeBubble();
-getNewHit();
